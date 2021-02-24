65dc88ef79218fdab58cf71d8ec8a9cae427b113
linghuchong <1951858651@qq.com>
2021-02-24 18:49:08 +0800

    修改1

---
+++ src/com.sun/java/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("新来第一天");
+        System.out.println("新来第2天");
+        System.out.println("新来第3天");
+        System.out.println("新来第4天");
+        System.out.println("新来第5天");
+        System.out.println("新来第6天");
+    }
+}
